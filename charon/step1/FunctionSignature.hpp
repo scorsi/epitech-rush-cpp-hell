82c0e09c6e471b6c4a9ea09e691aec4814a1da9f
scorsi <sylvain.corsini@gmail.com>
2018-01-05 23:05:26 +0100

    Step1

--- charon/step1/FunctionSignature.hpp
+++ charon/step1/FunctionSignature.hpp
@@ -5,9 +5,33 @@
 #ifndef STEP1_FUNCTIONSIGNATURE_HPP
 #define STEP1_FUNCTIONSIGNATURE_HPP
 
-template <typename T>
+template<typename T>
 struct FunctionSignature {
-    typedef T *type;
+};
+
+template<typename RetType>
+struct FunctionSignature<RetType()> {
+    typedef RetType (*type)();
+};
+
+template<typename RetType, typename ArgType1>
+struct FunctionSignature<RetType(ArgType1)> {
+    typedef RetType (*type)(ArgType1);
+};
+
+template<typename RetType, typename ArgType1, typename ArgType2>
+struct FunctionSignature<RetType(ArgType1, ArgType2)> {
+    typedef RetType (*type)(ArgType1, ArgType2);
+};
+
+template<typename RetType, typename ArgType1, typename ArgType2, typename ArgType3>
+struct FunctionSignature<RetType(ArgType1, ArgType2, ArgType3)> {
+    typedef RetType (*type)(ArgType1, ArgType2, ArgType3);
+};
+
+template<typename RetType, typename ArgType1, typename ArgType2, typename ArgType3, typename ArgType4>
+struct FunctionSignature<RetType(ArgType1, ArgType2, ArgType3, ArgType4)> {
+    typedef RetType (*type)(ArgType1, ArgType2, ArgType3, ArgType4);
 };
 
 #endif //STEP1_FUNCTIONSIGNATURE_HPP
--- charon/step1/main.cpp
+++ charon/step1/main.cpp
@@ -7,12 +7,56 @@
 #include "FunctionSignature.hpp"
 
 int display(const std::string &str) {
-    printf(str.c_str());
+    printf("%s\n", str.c_str());
     return 0;
 }
 
+void display2(int a) {
+    printf("%d\n", a);
+}
+
+void p() {
+    printf("no argument\n");
+}
+
+void pp(char *toto) {
+    printf("%c\n", *toto);
+}
+
+void pp2(char *toto, int *tata) {
+    printf("%c %c\n", *toto, (char) *tata);
+}
+
+void pp4(int a, int b, int c, int d) {
+    printf("%d %d %d %d\n", a, b, c, d);
+}
+
+void pp5(int a, int b, int c, int d, int e) {
+    printf("%d %d %d %d %d\n", a, b, c, d, e);
+}
+
 int main() {
-    FunctionSignature<int (const std::string &)>::type f = &display;
-    f("toto");
+    FunctionSignature<void()>::type f0 = &p;
+    f0();
+
+    FunctionSignature<int(const std::string &)>::type f1 = &display;
+    f1("toto");
+
+    FunctionSignature<void(int)>::type f2 = &display2;
+    f2(2);
+
+    FunctionSignature<void(char *)>::type f3 = &pp;
+    char f3_test = 't';
+    f3(&f3_test);
+
+    FunctionSignature<void(char *, int *)>::type f4 = &pp2;
+    int f4_test = 'o';
+    f4(&f3_test, &f4_test);
+
+    FunctionSignature<void(int, int, int, int)>::type f5 = &pp4;
+    f5(1, 2, 3, 4);
+
+    //FunctionSignature<void(int, int, int, int, int)>::type f6 = &pp5;
+    //f6(1, 2, 3, 4, 5);
     return 0;
 }
