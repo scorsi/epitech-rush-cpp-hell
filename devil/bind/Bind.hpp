15eaf64b63b03895282ef47fc390c6396e045946
scorsi <sylvain.corsini@gmail.com>
2018-01-07 06:20:20 +0100

    Exercise 3 Step 2

--- devil/bind/Bind.hpp
+++ devil/bind/Bind.hpp
@@ -5,4 +5,54 @@
 #ifndef BIND_BIND_HPP
 #define BIND_BIND_HPP
 
+#include "Storage.hpp"
+
+template<typename ReturnType, typename Callable, typename List>
+class Caller {
+private:
+    Callable _callable;
+    List _list;
+
+public:
+    Caller(const Callable &callable, const List &list) : _callable(callable), _list(list) {}
+
+    ReturnType operator()() {
+
+    }
+};
+
+class TypeList0 : private Storage0 {
+
+};
+
+template<typename T1>
+class TypeList1 : private Storage1<T1> {
+
+};
+
+template<typename T1, typename T2>
+class TypeList2 : private Storage2<T1, T2> {
+
+};
+
+template<typename T1, typename T2, typename T3>
+class TypeList3 : private Storage3<T1, T2, T3> {
+
+};
+
+template<typename T1, typename T2, typename T3, typename T4>
+class TypeList4 : private Storage4<T1, T2, T3, T4> {
+
+};
+
+template<typename T1, typename T2, typename T3, typename T4, typename T5>
+class TypeList5 : private Storage5<T1, T2, T3, T4, T5> {
+
+};
+
+template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
+class TypeList6 : private Storage6<T1, T2, T3, T4, T5> {
+
+};
+
 #endif //BIND_BIND_HPP
