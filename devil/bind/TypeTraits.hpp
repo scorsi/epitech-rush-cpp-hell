592bb1970095e62fc53db4727a5ff253822dfa4e
pillon_m <maxime.pillon@epitech.eu>
2018-01-07 05:42:17 +0100

    TypeTraits class

---
+++ devil/bind/TypeTraits.hpp
@@ -0,0 +1,29 @@
+//
+// Created by god on 1/7/18.
+//
+
+#ifndef BIND_TYPETRAITS_HPP
+#define BIND_TYPETRAITS_HPP
+
+template<typename T>
+class Value {
+private:
+  T &t;
+
+  T &get() { return this->t; }
+
+  T const &get() const { return  this->t; }
+ };
+
+template<class T>
+class GetType
+{
+public:
+  typedef Value<T> Type;
+};
+
+template<class T>
+struct TypeTraits {};
+};
+
+#endif //BIND_TYPETRAITS_HPP
